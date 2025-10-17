c9ef57fed40ee04142318d5189390bfdc6fe66a9
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-10-17 10:58:43 +0700

    Initial Project Setup

--- app/src/main/java/com/example/proyek1/MainActivity.kt
+++ app/src/main/java/com/example/proyek1/MainActivity.kt
@@ -4,23 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.proyek1.ui.theme.Proyek1Theme // Sesuaikan dengan nama tema Anda
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.proyek1.ui.theme.Proyek1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Proyek1Theme { // Sesuaikan dengan nama tema Anda
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                    // PANGGIL FUNGSINYA DI SINI
-                    TataletakBoxColumnRow(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-
+            Proyek1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    // Konten akan diubah
                 }
             }
         }
