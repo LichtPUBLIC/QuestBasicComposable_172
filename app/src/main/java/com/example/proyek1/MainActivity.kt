7016fb769a1ba351312230704064b2522aab1bf3
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-10-17 10:59:01 +0700

    Add Scaffold to MainActivity

--- app/src/main/java/com/example/proyek1/MainActivity.kt
+++ app/src/main/java/com/example/proyek1/MainActivity.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.proyek1.ui.theme.Proyek1Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,9 +14,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Proyek1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    // Konten akan diubah
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    // Composable utama akan dipanggil di sini
                 }
             }
         }
